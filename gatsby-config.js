484311b56ac1b0e085a4e67f3801d2018f44f5c9
enelso26 <erik.nelson@optum.com>
2019-10-28 10:29:35 -0500

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    title: `How I Dev`,
+    author: `Erik Nelson`,
+    description: `How do I dev? How do YOU dev?`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `ejnelson`,
     },
   },
   plugins: [
